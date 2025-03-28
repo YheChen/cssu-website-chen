7cde9ccc6ff6de4dde627c6f231b2cd5bb3b9ffb
YheChen <yanzhenchen123@gmail.com>
2025-03-28 12:45:28 -0400

    erasing posts logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={openSans.className}>
         <div className="flex flex-col min-h-screen">
           <header className="top-0 z-50 sticky">
